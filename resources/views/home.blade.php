a924b80299874048063dadbdb735723e3d8c2882
shlurkis <arminasrupsys03@gmail.com>
2024-10-06 11:01:38 +0300

    Created main page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<h1>Konferencijų Registracija</h1>
+<p>Vardas: Arminas</p>
+<p>Pavardė: Rupšys</p>
+<p>Grupė: PIT-22</p>
+<h2>Posistemiai</h2>
+<ul>
+    <li><a href="/client">Kliento posistemis</a></li>
+    <li><a href="/employee">Darbuotojo posistemis</a></li>
+    <li><a href="/admin">Administratoriaus posistemis</a></li>
+</ul>
